a48bde053fd80df46815b9d74d2ed9051403e2d3
yoldkwlws <lestheradonys@gmail.com>
2026-01-15 22:35:50 -0600

    Add files via upload

---
+++ monstruoserie.js
@@ -0,0 +1,27 @@
+// monstruoserie.js - Monstruo: La historia de Ed Gein
+window.seriesList = window.seriesList || [];
+
+window.seriesList.push({
+    id: 104,
+    title: "Monstruo: La historia de Ed Gein",
+    year: "2025",
+    image: "https://raw.githubusercontent.com/yoldkwlws/Stranger-things/refs/heads/main/2RVFksm4DF17XZSXMVbNh1qDSOr.jpg",
+    trailer: "https://www.youtube.com/embed/J20xarD-SxI?start=44",
+    info: "Wisconsin, años 50. Tras la muerte de su dominante madre, el solitario granjero Ed Gein comienza a hundirse en una espiral de locura y necrofilia. Lo que comienza como profanaciones de tumbas pronto se convierte en una serie de asesinatos atroces que conmocionan a la nación, revelando una colección macabra de trofeos humanos en su granja de Plainfield que cambiaría la historia de la criminología para siempre.",
+    featured: true,
+    seasons: [
+        {
+            season: 1,
+            episodes: [
+                { episode: 1, video: "https://vimeos.net/embed-63eijc4gy93j.html" },
+                { episode: 2, video: "https://vimeos.net/embed-v8ze3cgq5dd5.html" },
+                { episode: 3, video: "https://vimeos.net/embed-uqt5tcc1d7ae.html" },
+                { episode: 4, video: "https://vimeos.net/embed-mc41zfg05pgl.html" },
+                { episode: 5, video: "https://vimeos.net/embed-cddiigdkkupc.html" },
+                { episode: 6, video: "https://vimeos.net/embed-6nbmao7f5xqy.html" },
+                { episode: 7, video: "https://vimeos.net/embed-acw7ygk7hjux.html" },
+                { episode: 8, video: "https://vimeos.net/embed-xuspjdrkyt8s.html" }
+            ]
+        }
+    ]
+});
