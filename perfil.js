996453d20da8702ac1fdf1ac8cb0975b40413b11
yoldkwlws <lestheradonys@gmail.com>
2026-01-17 23:46:36 -0600

    Add files via upload

---
+++ perfil.js
@@ -0,0 +1,11 @@
+const profileImages = [
+    "https://raw.githubusercontent.com/yoldkwlws/It-Bienvenidos-a-Derry/refs/heads/main/18150fad27d6b1593a8bfb32d58cb6d1.png",
+    "https://raw.githubusercontent.com/yoldkwlws/It-Bienvenidos-a-Derry/refs/heads/main/2094d64189d9019bbc9090eea6b586a2.png",
+    "https://raw.githubusercontent.com/yoldkwlws/It-Bienvenidos-a-Derry/refs/heads/main/Picsart_26-01-17_23-20-17-367.jpg",
+    "https://raw.githubusercontent.com/yoldkwlws/It-Bienvenidos-a-Derry/refs/heads/main/Picsart_26-01-17_23-20-50-779(1).jpg",
+    "https://raw.githubusercontent.com/yoldkwlws/It-Bienvenidos-a-Derry/refs/heads/main/Picsart_26-01-17_23-20-50-779.jpg",
+    "https://raw.githubusercontent.com/yoldkwlws/It-Bienvenidos-a-Derry/refs/heads/main/Picsart_26-01-17_23-23-10-523.jpg",
+    "https://raw.githubusercontent.com/yoldkwlws/It-Bienvenidos-a-Derry/refs/heads/main/Picsart_26-01-17_23-25-52-270.jpg",
+    "https://raw.githubusercontent.com/yoldkwlws/It-Bienvenidos-a-Derry/refs/heads/main/Picsart_26-01-17_23-28-02-773.jpg",
+    "https://raw.githubusercontent.com/yoldkwlws/It-Bienvenidos-a-Derry/refs/heads/main/b1b0a4c792fd2d4ccad0c91d9a33531e.png"
+];
