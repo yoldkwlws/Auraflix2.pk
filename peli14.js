a67fef56e761d923ace4c9f4b27f5c96e6019379
yoldkwlws <lestheradonys@gmail.com>
2026-01-19 22:56:21 -0600

    Add files via upload

---
+++ peli14.js
@@ -0,0 +1,14 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p14",
+    title: "Noche de paz, noche de horror",
+    tags: ["Noche de Paz, Noche de Muerte", "SILENT NIGHT, DEADLY NIGHT"],
+    image: "https://raw.githubusercontent.com/yoldkwlws/Stranger-things/refs/heads/main/IMG_20260116_170404_248.jpg",
+    trailer: "https://www.youtube.com/embed/O8uiMiY52UY?start=3",
+    video: "https://vimeos.net/embed-avdelbv2qjz0.html",
+    year: 2025,
+    genres: ["Terror", "Suspenso"],
+    info: "Un hombre traumatizado por un evento violento en su infancia durante la Navidad sufre un colapso psicótico. Disfrazado de Santa Claus, emprende una sangrienta cruzada para castigar a los 'traviesos' en su pequeño pueblo, transformando la noche más tranquila del año en una auténtica pesadilla de supervivencia donde nadie está a salvo de su juicio.",
+    featured: true
+});
