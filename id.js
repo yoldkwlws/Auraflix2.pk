05435bb2b2547f348d71e0888a19e839b6ee5ff9
yoldkwlws <lestheradonys@gmail.com>
2026-01-22 16:55:22 -0600

    Add files via upload

---
+++ id.js
@@ -0,0 +1,22 @@
+window.HERO_IDS = [
+   {
+   id:"p12",
+    img:"https://raw.githubusercontent.com/yoldkwlws/Dr-cula/refs/heads/main/cf7hE1ifY4UNbS25tGnaTyyDrI2.jpg",
+   },
+    "p13",
+    "s2",
+    "p21",
+    "p4"
+];
+
+window.LATELY_IDS = [
+    "p20",
+    "p19",
+    "s3",
+    "p18",
+    "p17",
+    "s4",
+    "p16",
+    "p15",
+    "p14"
+];
