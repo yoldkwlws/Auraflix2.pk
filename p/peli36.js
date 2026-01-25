981b11c50ea3885872286d04742711480dadcb5d
Auraflix <lestheradonys@gmail.com>
2026-01-24 19:56:06 -0600

    Update movie title and tags for F1

--- p/peli36.js
+++ p/peli36.js
@@ -2,8 +2,8 @@ window.moviesList = window.moviesList || [];
 
 window.moviesList.push({
     id: "p36",
-    title: "F1",
-    tags: ["F1 la película", "F1 The movie"],
+    title: "F1 la película",
+    tags: ["F1 The movie"],
     image: "https://raw.githubusercontent.com/yoldkwlws/Stranger-things/refs/heads/main/yKIG63pXN89EfbTA7yKpwxAU1rf.jpg",
     trailer: "https://www.youtube.com/embed/AfhRi520IJw",
     video: "https://vimeos.net/embed-0f5b0dvl4iix.html",
