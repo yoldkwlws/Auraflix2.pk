50361d82353ca88b27effd33fa136918fca5b351
Auraflix <lestheradonys@gmail.com>
2026-01-24 19:50:17 -0600

    Add files via upload

---
+++ s/serie10.js
@@ -0,0 +1,29 @@
+window.seriesList = window.seriesList || [];
+
+window.seriesList.push({
+    id: "s10",
+    title: "Las brujas de Mayfair",
+    tags: ["Mayfair Witches"],
+    year: "2023",
+    genres: ["Drama", "Ciencia ficción", "Fantasía"],
+    image: "https://raw.githubusercontent.com/yoldkwlws/Stranger-things/refs/heads/main/iRM12uCbKq5BONNERukJWzZv1pE.jpg",
+    trailer: "https://www.youtube.com/embed/B98Qhf9wwJY",
+    info: "Rowan Fielding es una joven y brillante neurocirujana que descubre un secreto asombroso: es la heredera de una poderosa dinastía de brujas. Mientras lucha con sus nuevos y aterradores poderes, debe enfrentarse a una presencia siniestra que ha perseguido a su familia durante generaciones. Un drama sobrenatural que explora el legado, el destino y los oscuros secretos de Nueva Orleans.",
+    featured: true,
+    seasons: [
+        {
+            season: 1,
+            episodes: [
+                { episode: 1, video: "https://vimeos.net/embed-8pkybp0bh247.html" }, { episode: 2, video: "https://vimeos.net/embed-wbxdcd2ob1q9.html" }, { episode: 3, video: "https://vimeos.net/embed-8kgzgpn4ef46.html" }, { episode: 4, video: "https://vimeos.net/embed-f4tzwfk84ckm.html" },
+                { episode: 5, video: "https://vimeos.net/embed-jfqh3a3216ry.html" }, { episode: 6, video: "https://vimeos.net/embed-qeuld31dxlcn.html" }, { episode: 7, video: "https://vimeos.net/embed-gdfm8a9tx4k5.html" }, { episode: 8, video: "https://vimeos.net/embed-iry06bxx4m1b.html" }
+            ]
+        },
+        {
+            season: 2,
+            episodes: [
+                { episode: 1, video: "https://vimeos.net/embed-7z4t3afsdzkm.html" }, { episode: 2, video: "https://vimeos.net/embed-4bllslqj592u.html" }, { episode: 3, video: "https://vimeos.net/embed-opetn9b7miuv.html" }, { episode: 4, video: "https://vimeos.net/embed-sjcsuwipbf8d.html" },
+                { episode: 5, video: "https://vimeos.net/embed-wiuam70dnnlt.html" }, { episode: 6, video: "https://vimeos.net/embed-332w7tx3kmzx.html" }, { episode: 7, video: "https://vimeos.net/embed-rrr627nf45zb.html" }, { episode: 8, video: "https://vimeos.net/embed-d6sbuza4jon9.html" }
+            ]
+        }
+    ]
+});
