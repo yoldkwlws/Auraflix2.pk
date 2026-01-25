525dc881586d7cfab85e1df3a9c86e6760608a79
Auraflix <lestheradonys@gmail.com>
2026-01-24 19:50:03 -0600

    Add files via upload

---
+++ p/peli36.js
@@ -0,0 +1,14 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p36",
+    title: "F1",
+    tags: ["F1 la película", "F1 The movie"],
+    image: "https://raw.githubusercontent.com/yoldkwlws/Stranger-things/refs/heads/main/yKIG63pXN89EfbTA7yKpwxAU1rf.jpg",
+    trailer: "https://www.youtube.com/embed/AfhRi520IJw",
+    video: "https://vimeos.net/embed-0f5b0dvl4iix.html",
+    year: 2025,
+    genres: ["Acción", "Drama"],
+    info: "Un antiguo piloto de Fórmula 1 regresa al Gran Circo para competir en el equipo ficticio APXGP. Con el objetivo de mentorizar a una joven promesa del automovilismo y demostrar que aún tiene lo necesario para estar en la cima, se sumerge en una temporada llena de adrenalina, rivalidades extremas y la búsqueda de la gloria en las pistas más icónicas del mundo.",
+    featured: true
+});
---
+++ p/peli37.js
@@ -0,0 +1,14 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p37",
+    title: "ZOMBIES 4: El origen de los vampiros",
+    tags: ["Z-O-M-B-I-E-S 4: Un verano entre vampiros", "Z-O-M-B-I-E-S 4 DAWN OF THE VAMPIRES"],
+    image: "https://raw.githubusercontent.com/yoldkwlws/Stranger-things/refs/heads/main/jVGLT2ThsR9fh32ifKyn1gCrm2Z.jpg",
+    trailer: "https://www.youtube.com/embed/VFP7dJE6uxU?start=4",
+    video: "https://vimeos.net/embed-0qlsq4muguuc.html",
+    year: 2025,
+    genres: ["Fantasía", "Música", "Romance", "Aventura", "Familia", "Comedia", "Película de TV"],
+    info: "Zed y Addison comienzan su primer verano después de la universidad y emprenden un viaje para explorar nuevos mundos. En su aventura, se encuentran con dos nuevas facciones: los vampiros y los monstruos, quienes están en conflicto. Mientras intentan mantener la paz en este nuevo entorno, descubren secretos ancestrales sobre el origen de estas criaturas que cambiarán su percepción de Seabrook para siempre.",
+    featured: true
+});
