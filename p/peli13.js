322612997f7d1351460750dc2c6cac663bad61e4
Auraflix <lestheradonys@gmail.com>
2026-01-31 18:59:34 -0600

    Update peli13.js

--- p/peli13.js
+++ p/peli13.js
@@ -6,7 +6,7 @@ window.moviesList.push({
     tags: ["Zootrópolis 2"],
     image: "https://raw.githubusercontent.com/yoldkwlws/Stranger-things/refs/heads/main/bjUWGw0Ao0qVWxagN3VCwBJHVo6.jpg",
     trailer: "https://www.youtube.com/embed/iEYv6a945J8",
-    video: "https://vimeos.net/embed-mjskmrhlb91i.html",
+    video: "https://vimeos.net/embed-80qf4rbpdhhr.html",
     year: 2025,
     genres: ["Animación", "Comedia", "Aventura", "Aventura", "Familia", "Misterio"],
     info: "Los agentes Judy Hopps y Nick Wilde regresan a las calles de la metrópolis para investigar un caso que involucra a una misteriosa serpiente. Su búsqueda los llevará a descubrir una sociedad secreta de reptiles que vive en las sombras de la ciudad, poniendo a prueba su astucia y demostrando que, incluso en Zootopia, las apariencias pueden ser letales.",
