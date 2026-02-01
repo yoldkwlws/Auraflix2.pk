f9228cf7965909e230f023200bd06a8c1ebbb47b
Auraflix <lestheradonys@gmail.com>
2026-01-31 20:55:52 -0600

    Add files via upload

---
+++ id.js
@@ -0,0 +1,34 @@
+window.HERO_IDS = [
+   {
+   id:"p12",
+    img:"https://raw.githubusercontent.com/yoldkwlws/Stranger-things/refs/heads/main/cf7hE1ifY4UNbS25tGnaTyyDrI2(1).jpg",
+   },
+   {
+   id:"p13",
+   img:"https://raw.githubusercontent.com/yoldkwlws/Dr-cula/refs/heads/main/3Wg1LBCiTEXTxRrkNKOqJyyIFyF.jpg",
+   },
+   {
+    id:"s2",
+    img:"https://raw.githubusercontent.com/yoldkwlws/Dr-cula/refs/heads/main/2titW404dPYOzK5NElQoAJ2ivK5.jpg",
+   },
+   {
+    id:"p21",
+    img:"https://raw.githubusercontent.com/yoldkwlws/Dr-cula/refs/heads/main/lDL8vPUuJs2LsPHPjAGhkbYTpNX.jpg",
+   },
+   {
+    id:"p4",
+    img:"https://raw.githubusercontent.com/yoldkwlws/Dr-cula/refs/heads/main/9rtBxtUOYO03Pqi8JJbWt8z6cL5.jpg",
+   }
+];
+
+window.LATELY_IDS = [
+    "s12",
+    "s11",
+    "s3",
+    "p22",
+    "p38",
+    "p44",
+    "p1",
+    "p15",
+    "p14"
+];
