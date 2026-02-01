80398014fa6a07ecc9e9c571c1ed1118a7c0ea72
Auraflix <lestheradonys@gmail.com>
2026-01-31 20:56:58 -0600

    Add files via upload

---
+++ s/serie12.js
@@ -0,0 +1,31 @@
+window.seriesList = window.seriesList || [];
+
+window.seriesList.push({
+    id: "s12",
+    title: "Belleza perfecta",
+    tags: ["The Beauty"],
+    year: "2026",
+    genres: ["Drama", "Ciencia ficción", "Fantasía"],
+    image: "https://raw.githubusercontent.com/yoldkwlws/Stranger-things/refs/heads/main/tZTG2PPAukzksPkiQV9c4suIQXk.jpg",
+    trailer: "https://www.youtube.com/embed/3mmjJU4gNTk",
+    info: "En un futuro cercano, una enfermedad de transmisión sexual llamada 'The Beauty' transforma a quienes la contraen en personas físicamente perfectas. Sin embargo, lo que parece un milagro estético oculta una realidad aterradora: el virus también es una bomba de tiempo mortal. Dos detectives del FBI se adentran en una conspiración global para descubrir la verdad detrás de esta epidemia de perfección.",
+    featured: true,
+    seasons: [
+        {
+            season: 1,
+            episodes: [
+                { episode: 1, video: "https://vimeos.net/embed-apiitzmvvphx.html" },
+                { episode: 2, video: "https://vimeos.net/embed-tzmyl8yymnnk.html" },
+                { episode: 3, video: "https://vimeos.net/embed-0apzfxed5mm9.html" },
+                // { episode: 4, video: "x" },
+                // { episode: 5, video: "x" },
+                // { episode: 6, video: "x" },
+                // { episode: 7, video: "x" },
+                // { episode: 8, video: "x" },
+                // { episode: 9, video: "x" },
+                // { episode: 10, video: "x" },
+                // { episode: 11, video: "x" }
+            ]
+        }
+    ]
+});
