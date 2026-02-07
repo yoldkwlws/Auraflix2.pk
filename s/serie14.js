139d74409c512ec8e8cbe66dedb25ae86214ae34
Auraflix <lestheradonys@gmail.com>
2026-02-06 23:03:30 -0600

    Add files via upload

---
+++ s/serie14.js
@@ -0,0 +1,31 @@
+window.seriesList = window.seriesList || [];
+
+window.seriesList.push({
+    id: "s14",
+    title: "Insatiable",
+    tags: ["insaciable"],
+    year: "2018",
+    genres: ["Drama", "Comedia"],
+    image: "https://raw.githubusercontent.com/yoldkwlws/Stranger-things/refs/heads/main/lHZ4xqGQlmyiFTOVtwnNpTcZgkd%20(1).jpg",
+    trailer: "https://www.youtube.com/embed/z-81WVD8xTs",
+    info: "Patty Bladell ha sido acosada y subestimada durante años debido a su peso. Tras un accidente que la obliga a hacer una dieta líquida y perder peso, regresa a la escuela buscando venganza. Con la ayuda de Bob Armstrong, un abogado caído en desgracia apasionado por los concursos de belleza, Patty se convierte en una competidora feroz, demostrando que la venganza es un plato que se sirve con corona.",
+    featured: true,
+    seasons: [
+        {
+            season: 1,
+            episodes: [
+                { episode: 1, video: "https://player.vimeo.com/video/1162726588" }, { episode: 2, video: "https://player.vimeo.com/video/1162723728" }, { episode: 3, video: "https://player.vimeo.com/video/1162729376" }, { episode: 4, video: "https://player.vimeo.com/video/1162731570" },
+                { episode: 5, video: "https://player.vimeo.com/video/1162733492" }, { episode: 6, video: "https://player.vimeo.com/video/1162735826" }, { episode: 7, video: "https://player.vimeo.com/video/1162736826" }, { episode: 8, video: "https://player.vimeo.com/video/1162738992" },
+                { episode: 9, video: "https://player.vimeo.com/video/1162740483" }, { episode: 10, video: "https://player.vimeo.com/video/1162744033" }, { episode: 11, video: "https://player.vimeo.com/video/1162744778" }, { episode: 12, video: "https://player.vimeo.com/video/1162746759" }
+            ]
+        },
+        {
+            season: 2,
+            episodes: [
+                { episode: 1, video: "https://player.vimeo.com/video/1162723818" }, { episode: 2, video: "https://player.vimeo.com/video/1162725513" }, { episode: 3, video: "https://player.vimeo.com/video/1162730829" }, { episode: 4, video: "https://player.vimeo.com/video/1162732820" },
+                { episode: 5, video: "https://player.vimeo.com/video/1162735864" }, { episode: 6, video: "https://player.vimeo.com/video/1162736859" }, { episode: 7, video: "https://player.vimeo.com/video/1162739037" }, { episode: 8, video: "https://player.vimeo.com/video/1162740416" },
+                { episode: 9, video: "https://player.vimeo.com/video/1162744824" }, { episode: 10, video: "https://player.vimeo.com/video/1162744001" }
+            ]
+        }
+    ]
+});
