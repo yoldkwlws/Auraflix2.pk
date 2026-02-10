7dbf87d472721a215fbb761704d3ff59a9a72a8c
Auraflix <lestheradonys@gmail.com>
2026-02-10 00:38:48 -0600

    Add files via upload

---
+++ p/peli67.js
@@ -0,0 +1,13 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p67",
+    title: "Together: Juntos hasta la muerte",
+    image: "https://raw.githubusercontent.com/yoldkwlws/Stranger-things/refs/heads/main/h74kQxw1khjTwBbT2Rj1vACRh1k.jpg",
+    trailer: "https://www.youtube.com/embed/Pb9tln--Ajs",
+    video: "https://vimeos.net/embed-bctb56wi6xca.html",
+    year: 2025,
+    genres: ["Terror", "Romance"],
+    info: "En un futuro post-apocalíptico, una pareja lucha por sobrevivir en una cabaña aislada mientras una presencia externa acecha. Sin embargo, el verdadero peligro surge cuando descubren que para mantenerse a salvo, deben permanecer físicamente unidos por un vínculo inquebrantable, poniendo a prueba su cordura y su voluntad de sacrificio en un pacto de vida o muerte.",
+    featured: true
+});
