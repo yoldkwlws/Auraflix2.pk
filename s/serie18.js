8270181d5080449b14765d30858c437bd9ab3723
Auraflix <lestheradonys@gmail.com>
2026-02-11 14:51:29 -0600

    Add files via upload

---
+++ s/serie18.js
@@ -0,0 +1,46 @@
+window.seriesList = window.seriesList || [];
+
+window.seriesList.push({
+    id: "s18",
+    title: "The Umbrella Academy",
+    tags: ["academia"],
+    year: "2019",
+    genres: ["Acción", "Aventura", "Ciencia ficción", "Fantasía", "Drama"],
+    image: "https://raw.githubusercontent.com/yoldkwlws/Stranger-things/refs/heads/main/uYHdIs5O8tiU5p6MvUPd2jElOH6.jpg",
+    trailer: "https://www.youtube.com/embed/0DAmWHxeoKw",
+    info: "Una familia disfuncional de superhéroes se reúne para resolver el misterio de la muerte de su padre y la amenaza de un apocalipsis inminente. Basada en los cómics de Gerard Way, la serie sigue a los hermanos Hargreeves mientras viajan por el tiempo y enfrentan realidades alternativas para salvar el mundo.",
+    featured: true,
+    seasons: [
+        {
+            season: 1,
+            episodes: [
+                { episode: 1, video: "https://vimeos.net/embed-enpejvcyolsg.html" }, { episode: 2, video: "https://vimeos.net/embed-nxoccjp96n4p.html" }, { episode: 3, video: "https://vimeos.net/embed-dtexvg7k4kaf.html" }, { episode: 4, video: "https://vimeos.net/embed-7at1x4oduv6j.html" },
+                { episode: 5, video: "https://vimeos.net/embed-mpzukjt8ayzg.html" }, { episode: 6, video: "https://vimeos.net/embed-0epsm4775870.html" }, { episode: 7, video: "https://vimeos.net/embed-foptkabsnn0j.html" }, { episode: 8, video: "https://vimeos.net/embed-ehuvffgyzsfl.html" },
+                { episode: 9, video: "https://vimeos.net/embed-qypparxhynae.html" }, { episode: 10, video: "https://vimeos.net/embed-0nrao3xjtyx1.html" }
+            ]
+        },
+        {
+            season: 2,
+            episodes: [
+                { episode: 1, video: "https://vimeos.net/embed-ktqrl424lf6v.html" }, { episode: 2, video: "https://vimeos.net/embed-ob4qw0wnujtd.html" }, { episode: 3, video: "https://vimeos.net/embed-juyjikw1d3cj.html" }, { episode: 4, video: "https://vimeos.net/embed-1gk3cvvoio72.html" },
+                { episode: 5, video: "https://vimeos.net/embed-xz90o4numfaj.html" }, { episode: 6, video: "https://vimeos.net/embed-ud51ov5tmhaf.html" }, { episode: 7, video: "https://vimeos.net/embed-wj9k3qu4lars.html" }, { episode: 8, video: "https://vimeos.net/embed-j86f4ugj0gkq.html" },
+                { episode: 9, video: "https://vimeos.net/embed-o6yhmoy5l8pn.html" }, { episode: 10, video: "https://vimeos.net/embed-79c0s0vtt8pl.html" }
+            ]
+        },
+        {
+            season: 3,
+            episodes: [
+                { episode: 1, video: "https://vimeos.net/embed-qovdg65kkyyj.html" }, { episode: 2, video: "https://vimeos.net/embed-r5gcmlmbdzle.html" }, { episode: 3, video: "https://vimeos.net/embed-iyjdsminwfai.html" }, { episode: 4, video: "https://vimeos.net/embed-cfi8rmed1ldz.html" },
+                { episode: 5, video: "https://vimeos.net/embed-fkmjleh85rbh.html" }, { episode: 6, video: "https://vimeos.net/embed-75ppvzqcayvo.html" }, { episode: 7, video: "https://vimeos.net/embed-9ikz2b3hhuq2.html" }, { episode: 8, video: "https://vimeos.net/embed-dmtzn2s428ek.html" },
+                { episode: 9, video: "https://vimeos.net/embed-ijv7z0bwpr5s.html" }, { episode: 10, video: "https://vimeos.net/embed-4fi5dh3i5e1t.html" }
+            ]
+        },
+        {
+            season: 4,
+            episodes: [
+                { episode: 1, video: "https://vimeos.net/embed-iaq7bl16d2ri.html" }, { episode: 2, video: "https://vimeos.net/embed-a977sujii7e6.html" }, { episode: 3, video: "https://vimeos.net/embed-e2c2a8hs6nij.html" }, 
+                { episode: 4, video: "https://vimeos.net/embed-7j8lqoc3goxn.html" }, { episode: 5, video: "https://vimeos.net/embed-t88fv4jknqvk.html" }, { episode: 6, video: "https://vimeos.net/embed-phwaf6gjl05j.html" }
+            ]
+        }
+    ]
+});
