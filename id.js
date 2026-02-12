4f102850310965a60a19c427c17b88c18fb5812a
Auraflix <lestheradonys@gmail.com>
2026-02-12 00:15:24 -0600

    Update id.js

--- id.js
+++ id.js
@@ -1,23 +1,23 @@
 window.HERO_IDS = [
    {
-   id:"p12",
+   id:"",
+    img:"",
+   },
+   {
+   id:"",
+   img:"",
+   },
+   {
+    id:"",
+    img:"",
+   },
+   {
+    id:"p12",
     img:"https://raw.githubusercontent.com/yoldkwlws/Stranger-things/refs/heads/main/cf7hE1ifY4UNbS25tGnaTyyDrI2(1).jpg",
    },
    {
-   id:"p13",
-   img:"https://raw.githubusercontent.com/yoldkwlws/Dr-cula/refs/heads/main/3Wg1LBCiTEXTxRrkNKOqJyyIFyF.jpg",
-   },
-   {
-    id:"s2",
-    img:"https://raw.githubusercontent.com/yoldkwlws/Dr-cula/refs/heads/main/2titW404dPYOzK5NElQoAJ2ivK5.jpg",
-   },
-   {
-    id:"p21",
-    img:"https://raw.githubusercontent.com/yoldkwlws/Dr-cula/refs/heads/main/Screenshot_20260122_200011_Moviebase(1).jpg",
-   },
-   {
-    id:"p4",
-    img:"https://raw.githubusercontent.com/yoldkwlws/Dr-cula/refs/heads/main/9rtBxtUOYO03Pqi8JJbWt8z6cL5.jpg",
+    id:"p13",
+    img:"https://raw.githubusercontent.com/yoldkwlws/Dr-cula/refs/heads/main/3Wg1LBCiTEXTxRrkNKOqJyyIFyF.jpg",
    }
 ];
 
