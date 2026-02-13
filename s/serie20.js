079bfe1baa948c578bcd8a983b100947504f64f5
Auraflix <lestheradonys@gmail.com>
2026-02-13 17:10:44 -0600

    Add files via upload

---
+++ s/serie20.js
@@ -0,0 +1,38 @@
+window.seriesList = window.seriesList || [];
+
+window.seriesList.push({
+    id: "s20",
+    title: "From",
+    year: "2022",
+    genres: ["Drama", "Misterio", "Ciencia ficción", "Fantasía"],
+    image: "https://raw.githubusercontent.com/Auraflixs/Stranger-things/refs/heads/main/cjXLrg4R7FRPFafvuQ3SSznQOd9.jpg",
+    trailer: "https://www.youtube.com/embed/e69AWLED77w",
+    info: "Un pueblo de pesadilla en el centro de EE. UU. atrapa a todos los que entran. Mientras los residentes luchan por mantener una sensación de normalidad y buscan una salida, también deben sobrevivir a las amenazas del bosque circundante, incluidas las aterradoras criaturas que salen cuando se pone el sol.",
+    featured: true,
+    seasons: [
+        {
+            season: 1,
+            episodes: [
+                { episode: 1, video: "https://vimeos.net/embed-u36ubokvgjri.html" }, { episode: 2, video: "https://vimeos.net/embed-9myc82m0rk5d.html" }, { episode: 3, video: "https://vimeos.net/embed-wg7ny092vfeh.html" }, 
+                { episode: 4, video: "https://vimeos.net/embed-7k9avgfuyljv.html" }, { episode: 5, video: "https://vimeos.net/embed-5k11bpdfu1vn.html" }, { episode: 6, video: "https://vimeos.net/embed-smh7pyhuxe0f.html" },
+                { episode: 7, video: "https://vimeos.net/embed-5ipk7bz0t91u.html" }, { episode: 8, video: "https://vimeos.net/embed-lfoaggvz5htp.html" }, { episode: 9, video: "https://vimeos.net/embed-0d93syy2odis.html" }, { episode: 10, video: ,"https://vimeos.net/embed-atwqpohpwgfo.html" }
+            ]
+        },
+        {
+            season: 2,
+            episodes: [
+                { episode: 1, video: "https://vimeos.net/embed-vikui78d5xda.html" }, { episode: 2, video: "https://vimeos.net/embed-d9w29bmir9gm.html" }, { episode: 3, video: "https://vimeos.net/embed-702zk3widf4d.html" }, 
+                { episode: 4, video: "https://vimeos.net/embed-9ifv70lakq87.html" }, { episode: 5, video: "https://vimeos.net/embed-n4bizpif5kye.html" }, { episode: 6, video: "https://vimeos.net/embed-s84aw0gmm0wb.html" },
+                { episode: 7, video: "https://vimeos.net/embed-xh0u9w55674v.html" }, { episode: 8, video: "https://vimeos.net/embed-xltw0yxuin0g.html" }, { episode: 9, video: "https://vimeos.net/embed-pymxdmbkw5jl.html" }, { episode: 10, video: "https://vimeos.net/embed-m2lyd3x8wiaj.html" }
+            ]
+        },
+        {
+            season: 3,
+            episodes: [
+                { episode: 1, video: "https://vimeos.net/embed-vzc4mxbnu9kq.html" }, { episode: 2, video: "https://vimeos.net/embed-gb58i89au8zr.html" }, { episode: 3, video: "https://vimeos.net/embed-scj1s8o66rxn.html" }, 
+                { episode: 4, video: "https://vimeos.net/embed-rckyra7j0cy3.html" }, { episode: 5, video: "https://vimeos.net/embed-ru0dg4y7r2mi.html" }, { episode: 6, video: "https://vimeos.net/embed-ltmcmumy9jn2.html" },
+                { episode: 7, video: "https://vimeos.net/embed-bwilmp2stklv.html" }, { episode: 8, video: "https://vimeos.net/embed-9wkuxuc1bb2j.html" }, { episode: 9, video: "https://vimeos.net/embed-88qqrwjf70x1.html" }, { episode: 10, video: "https://vimeos.net/embed-d23ojpqmyng6.html" }
+            ]
+        }
+    ]
+});
