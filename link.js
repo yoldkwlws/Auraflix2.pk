a6ee8bd67ae3e48a95892ff1afa8ec154f9c3bf4
Auraflix <lestheradonys@gmail.com>
2026-02-12 23:17:40 -0600

    Update link.js

--- link.js
+++ link.js
@@ -1,6 +1,6 @@
 const AURA_LINKS = {
     // Enlace del Grupo de Telegram (Botón Difusión)
-    telegram: 'https://t.me/Auraflixpeli',
+    telegram: 'https://t.me/Aold9kuraflixpeli',
 
     // Enlace para descargar la App (Botón Compartir App)
     appDownload: 'https://www.mediafire.com/file/gipq3eknebz7lkn/peli49292.apk/file',
