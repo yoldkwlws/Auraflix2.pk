66ea4e5f9db1c6c7f2dcc2c93ab52462915ec0b2
Auraflix <lestheradonys@gmail.com>
2026-02-13 11:04:49 -0600

    Add files via upload

---
+++ p/peli71.js
@@ -0,0 +1,14 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p71",
+    title: "El Payaso del Maizal",
+    tags: ["Clown in a Cornfield", "Cosecha sangrienta"],
+    image: "https://raw.githubusercontent.com/yoldkwlws/Stranger-things/refs/heads/main/diPSE2jVqMQF1g6u1duFzzxMxh9.jpg",
+    trailer: "https://www.youtube.com/embed/e69AWLED77w",
+    video: "https://vimeos.net/embed-m887djer2w4n.html",
+    year: 2025,
+    genres: ["Terror", "Suspenso", "Misterio"],
+    info: "Un grupo de adolescentes se pierde en un extenso laberinto de maíz durante las celebraciones de Halloween. Lo que parece un juego divertido se convierte en una pesadilla sangrienta cuando descubren que una entidad malévola disfrazada de payaso los acecha desde las sombras. Sin salida a la vista, deberán descifrar el origen de esta leyenda local para sobrevivir a la noche.",
+    featured: true
+});
