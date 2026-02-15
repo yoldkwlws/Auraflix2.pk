b89b2159703c7ea4fe71f7a0edda11b2a736b37c
Auraflix <lestheradonys@gmail.com>
2026-02-14 21:40:50 -0600

    Add files via upload

---
+++ s/serie21.js
@@ -0,0 +1,31 @@
+window.seriesList = window.seriesList || [];
+
+window.seriesList.push({
+    id: "s21",
+    title: "En el barro",
+    tags: ["in the mud"],
+    year: "2025",
+    genres: ["Crimen", "Drama"],
+    image: "https://raw.githubusercontent.com/Auraflixs/Stranger-things/refs/heads/main/kKi1YdzQNM87Mcz7WxxclHbevwr.jpg",
+    trailer: "https://www.youtube.com/embed/Ith7z6FzLe0",
+    info: "Cinco reclusas forjan un vínculo único tras un accidente mortal, hasta que la corrupción y las guerras territoriales dentro de una prisión despiadada amenazan con destruirlas. La historia se expande en su segunda temporada profundizando en el caos carcelario.",
+    featured: true,
+    seasons: [
+        {
+            season: 1,
+            episodes: [
+                { episode: 1, video: "https://dropload.pro/e/u6srudk0gk4l" }, { episode: 2, video: "https://dropload.pro/e/252am9hwzzrs" }, { episode: 3, video: "https://dropload.pro/e/ojkjukoabmvr" },
+                { episode: 4, video: "https://dropload.pro/e/f4wzhne23ax5" }, { episode: 5, video: "https://dropload.pro/e/01wv4nuxhzon" }, { episode: 6, video: "https://dropload.pro/e/22qu2o1vklcb" },
+                { episode: 7, video: "https://dropload.pro/e/hwo5shmjqqh4" }, { episode: 8, video: "https://dropload.pro/e/8xodu2zdymxs" }
+            ]
+        },
+        {
+            season: 2,
+            episodes: [
+                { episode: 1, video: "https://dropload.pro/e/jtd1ijntqh6y" }, { episode: 2, video: "https://dropload.pro/e/gx5l1xox5b6n" }, { episode: 3, video: "https://dropload.pro/e/b8w2g1h8rl7o" },
+                { episode: 4, video: "https://dropload.pro/e/jfvfnnu8f36f" }, { episode: 5, video: "https://dropload.pro/e/v10vrq6c11nt" }, { episode: 6, video: "https://dropload.pro/e/tajijws15dyb" },
+                { episode: 7, video: "https://dropload.pro/e/97omt13bafkv" }, { episode: 8, video: "https://dropload.pro/e/ea7cw6sim6u2" }
+            ]
+        }
+    ]
+});
