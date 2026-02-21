8c70531786e9193fef3b0d4008c09c52dd13862b
Auraflix <lestheradonys@gmail.com>
2026-02-20 21:47:50 -0600

    Add files via upload

---
+++ s/serie1.js
@@ -0,0 +1,29 @@
+window.seriesList = window.seriesList || [];
+
+window.seriesList.push({
+    id: "s1",
+    title: "Monstruo: La historia de Ed Gein",
+    tags: ["MONSTER: The Ed Gein Story"],
+    year: "2025",
+    genres: ["Drama", "Crimen"],
+    image: "https://raw.githubusercontent.com/Auraflixs/im-genestv/refs/heads/main/1v9X7qviwTllikhIZdVAjRmDQtL.webp",
+    trailer: "https://www.youtube.com/embed/J20xarD-SxI?start=44",
+    info: "Wisconsin, años 50. Tras la muerte de su dominante madre, el solitario granjero Ed Gein comienza a hundirse en una espiral de locura y necrofilia. Lo que comienza como profanaciones de tumbas pronto se convierte en una serie de asesinatos atroces que conmocionan a la nación, revelando una colección macabra de trofeos humanos en su granja de Plainfield que cambiaría la historia de la criminología para siempre.",
+    featured: true,
+    seasons: [
+        {
+            season: 1,
+            episodes: [
+                { episode: 1, video: "https://dropload.pro/e/2xxmrkoet8sq" },
+                { episode: 2, video: "https://dropload.pro/e/bq0gnnbk2enh" },
+                { episode: 3, video: "https://dropload.pro/e/ef8ckwndafuw" },
+                { episode: 4, video: "https://dropload.pro/e/af5friq15uxf" },
+                { episode: 5, video: "https://dropload.pro/e/j3eonvqmrl63" },
+                { episode: 6, video: "https://dropload.pro/e/238cc64zmdca" },
+                { episode: 7, video: "https://dropload.pro/e/3h72d1bhfcal" },
+                { episode: 8, video: "https://dropload.pro/e/kw1x6hii3fsu" }
+            ]
+        }
+    ]
+});
+
