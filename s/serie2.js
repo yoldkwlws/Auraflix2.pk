203cb1bb9afaa153a682b7fa09af8404fa30dd82
Auraflix <lestheradonys@gmail.com>
2026-02-20 22:23:40 -0600

    Add files via upload

---
+++ s/serie2.js
@@ -0,0 +1,28 @@
+window.seriesList = window.seriesList || [];
+
+window.seriesList.push({ 
+    id: "s2", 
+    title: "It: Bienvenidos a Derry", 
+    tags: ["It: Welcome to Derry"],
+    image: "https://raw.githubusercontent.com/Auraflixs/im-genestv/refs/heads/main/lOBHd34cIsPEgnkShI6BWEW7ZPU.webp", 
+    trailer: "https://www.youtube.com/embed/UI3AvYrIooQ", 
+    year: 2025, 
+    genres: ["Drama", "Misterio"],
+    info: 'Siguiendo los acontecimientos de la década de 1960, el período previo a los acontecimientos de la primera película de la serie "It" de Stephen King.', 
+    featured: true, 
+    seasons: [
+        { 
+            season: 1, 
+            episodes: [
+                { episode: 1, video: "https://dropload.pro/e/pms3gw1k624n" },
+                { episode: 2, video: "https://dropload.pro/e/96pun7xungwk" },
+                { episode: 3, video: "https://dropload.pro/e/m5cs8a3r1rzu" },
+                { episode: 4, video: "https://dropload.pro/e/3nvbwtwt4t0n" },
+                { episode: 5, video: "https://dropload.pro/e/j3yjqe0lvisv" },
+                { episode: 6, video: "https://dropload.pro/e/cmhfk7rv8x04" },
+                { episode: 7, video: "https://dropload.pro/e/4w8f8zibq4n4" },
+                { episode: 8, video: "https://dropload.pro/e/6jm3ahfaw0qa" }
+            ] 
+        }
+    ]
+});
